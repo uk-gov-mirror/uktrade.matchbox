57988e9238c36e5ae6c914377c4889d49d744cb9
Will Langdale <will.langdale@businessandtrade.gov.uk>
2026-08-11 16:43:10 +0100

    refactor: removed unnecesary reexports
    Signed-off-by: DBT pre-commit check

--- src/matchlab/eval/__init__.py
+++ src/matchlab/eval/__init__.py
@@ -1,23 +1,11 @@
 """Public evaluation helpers."""
 
-from matchlab.eval.samples import (
-    EvalData,
-    EvaluationFieldMetadata,
-    EvaluationItem,
-    create_evaluation_item,
-    create_judgement,
-    get_samples,
-    precision_recall,
-)
+from matchlab.eval.samples import EvalData, create_judgement, get_samples
 from matchlab.eval.tui import review
 
 __all__ = [
     "EvalData",
-    "EvaluationFieldMetadata",
-    "EvaluationItem",
-    "create_evaluation_item",
     "create_judgement",
     "get_samples",
-    "precision_recall",
     "review",
 ]
--- src/matchlab/resolvers/__init__.py
+++ src/matchlab/resolvers/__init__.py
@@ -1,15 +1,10 @@
 """Resolver methodologies and the Resolver plan node."""
 
-from matchlab.resolvers.base import ResolverMethod, ResolverSettings, ResolverType
 from matchlab.resolvers.components import Components, ComponentsSettings
-from matchlab.resolvers.resolvers import Resolver, add_resolver_class
+from matchlab.resolvers.resolvers import Resolver
 
 __all__ = (
     "Components",
     "ComponentsSettings",
     "Resolver",
-    "ResolverMethod",
-    "ResolverSettings",
-    "ResolverType",
-    "add_resolver_class",
 )
